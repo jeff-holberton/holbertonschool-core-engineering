2e633a0cfb714a584fbfe1e7816502c21f9048cb
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-09-21 17:32:31 +1000

    added return

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 
 def print_last_digit(number):
-    print(number % 10 if number >= 0 else (number % 10 - 10) * -1, end="")
+    last_digit = number % 10 if number >= 0 else (number % 10 - 10) * -1
+    print("{}".format(last_digit), end="")
+    return last_digit
