313ab2407486582a6e724338ba8a13a03e647f1a
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-09-21 15:42:08 +1000

    alphabet

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-for i in alphabet:
-    if i != 'e' or 'q':
-        print(i, end="")
-print()
+alphabet = "abcdfghijklmnoprstuvwxyz"
+print(alphabet)
