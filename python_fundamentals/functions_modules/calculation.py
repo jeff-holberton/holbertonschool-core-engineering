a26f60bd133fe92b7b6f130171be66e4ed4f30e3
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-09-21 18:24:30 +1000

    test 1

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from calculator_1 import add
+from calculator_1 import sub
+from calculator_1 import mul
+from calculator_1 import div
+
+
+def main():
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+
+
+if __name__ == "__main__":
+    main()
