ed8f105685907534324ebeac7bb49415a5c2d204
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-09-21 15:58:01 +1000

    f removed added .format

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -3,6 +3,6 @@
 for i in range(9):
     for j in range(i + 1, 10):
         if i != 8:
-            print(f"{i}{j}, ", end="")
+            print("{}{}, ".format(i, j), end="")
         else:
-            print(f"{i}{j}")
+            print("{i}{j}".format(i, j))
