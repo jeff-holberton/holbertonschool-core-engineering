dc467a7d45f75686b0e684ae8dde2b26058e7333
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-09-21 12:40:58 +1000

    Added first script

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+language = "Python"
+version = 3
+pi = 3.14
+computation = True
+print(f"Language: {language}")
+print(f"Version: {version}")
+print(f"Pi approx: {pi}")
+print(f"Computation valid: {computation}")
