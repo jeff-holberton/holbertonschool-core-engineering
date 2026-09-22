eac95241fa1d63f66feac12fdcb71101af8e108d
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-09-22 12:10:48 +1000

    Added guard clause

--- python_fundamentals/core_data_structures/add_tuple.py
+++ python_fundamentals/core_data_structures/add_tuple.py
@@ -3,8 +3,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a = [0, 0]
     list_b = [0, 0]
-    for index, element in enumerate(tuple_a):
+    for index, element in enumerate(tuple_a) and index < 2:
         list_a[index] = element
-    for index, element in enumerate(tuple_b):
+    for index, element in enumerate(tuple_b) and index < 2:
         list_b[index] = element
     return (list_a[0] + list_b[0], list_a[1] + list_b[1])
