3eb829dc920ad9d1e74396a4dbd76e08bb0d342b
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-09-22 12:13:00 +1000

    fixed guard

--- python_fundamentals/core_data_structures/add_tuple.py
+++ python_fundamentals/core_data_structures/add_tuple.py
@@ -3,8 +3,12 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a = [0, 0]
     list_b = [0, 0]
-    for index, element in enumerate(tuple_a) and index < 2:
+    for index, element in enumerate(tuple_a):
+        if index == 2:
+            break
         list_a[index] = element
-    for index, element in enumerate(tuple_b) and index < 2:
+    for index, element in enumerate(tuple_b):
+        if index == 2:
+            break
         list_b[index] = element
     return (list_a[0] + list_b[0], list_a[1] + list_b[1])
