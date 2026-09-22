f870eb90a9ed1a81d9de279bfed32a849fc7f823
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-09-22 12:01:36 +1000

    Added Null print

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print()
     for list in matrix:
         for index, element in enumerate(list):
             if index != len(list) - 1:
