8a08abdbd6548fd3dea7ebecf18b4218c7c10120
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-09-22 12:08:13 +1000

    No new line for Null

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     if not matrix or not matrix[0]:
-        print(" ")
+        print(" ", end="")
     for list in matrix:
         for index, element in enumerate(list):
             if index != len(list) - 1:
